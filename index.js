a8bef303fd609444a05c7c18e71fbc4fa75e8da3
Derek Christensen <covfefe24@protonmail.com>
2019-05-03 09:54:41 -0400

    testing more

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const categoriesController = require('./controllers/categories')
 const bodyParser = require('body-parser')
 
 
-
+app.listen(app.get("port"), () => {
+    console.log(`✅ PORT: ${app.get("port")} 🌟`)
+  })
+  
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
@@ -19,9 +22,7 @@ app.use('/api/categories/', categoriesController)
 
 app.set("port", process.env.PORT || 3001)
 
-app.listen(app.get("port"), () => {
-  console.log(`✅ PORT: ${app.get("port")} 🌟`)
-})
+
 
 // app.listen(8081, () => console.log('They see me rollin...on port 8081...'))
 
